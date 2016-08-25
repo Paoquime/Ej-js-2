f325376968a6e8e47baa805a9e5c7fa31b4f2ef5
Paoquime <paola.qmelendez@gmail.com>
2016-08-25 15:56:50 -0500

    Agregando ej-2

---
+++ main.js
@@ -0,0 +1,5 @@
+var numero1 = parseInt(prompt("numero1"));
+var numero2 = parseInt(prompt("numero2"));
+var numero3 = parseInt(prompt("numero3"));
+
+document.write((numero1 + numero2 + numero3 )/3);
